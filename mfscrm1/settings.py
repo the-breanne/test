96a13af693165b0d8c3ad252f12937d1496b4c99
the-breanne <77354893+the-breanne@users.noreply.github.com>
2021-04-15 17:34:40 -0500

    Update settings.py

--- mfscrm1/settings.py
+++ mfscrm1/settings.py
@@ -148,7 +148,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # This is now a string
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
